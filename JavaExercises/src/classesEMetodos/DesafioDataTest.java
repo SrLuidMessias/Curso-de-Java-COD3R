0732ed98c3c747181e13f29effcdf00e59d0d94c
Sr.Messias <121517890+SrLuidMessias@users.noreply.github.com>
2024-02-15 11:31:08 -0300

    Add files via upload

---
+++ JavaExercises/src/classesEMetodos/DesafioDataTest.java
@@ -0,0 +1,54 @@
+package classesEMetodos;
+import java.util.Date;
+import java.lang.reflect.Array;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Scanner;
+public class DesafioDataTest {
+
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		
+		DesafioData p1 = new DesafioData();
+		
+		System.out.println("Escreva seu dia de nascimento ->");
+		p1.dia = entrada.nextInt();
+		
+		System.out.println("Escreva seu Mês de nascimento ->");
+		p1.mes = entrada.nextInt();
+		
+		System.out.println("Escreva seu ano de nascimento ->");
+		p1.ano = entrada.nextInt();
+		
+		
+		Date date = new Date();
+		Calendar calendar = new GregorianCalendar();
+		calendar.setTime(date);
+		int anoAtual = calendar.get(Calendar.YEAR);
+		
+
+		System.out.printf("nascimento %d/%d/%d \n",p1.dia,p1.mes,p1.ano);
+        System.out.printf("Você tem %d anos \n",anoAtual - p1.ano);
+		
+        
+        String [] meses = {"Janeiro", "Fevereiro","Março","Abril", "Maio","junho"
+        		,"julho","Agosto","Setembro","Outubro","Novembro","Dezembro"};
+        
+     
+        System.out.printf("Nasceu em %s", (p1.mes == 1 ? meses[1]: 0) );
+        System.out.printf("Nasceu em %s", (p1.mes == 2 ? meses[2]: 0) );
+        System.out.printf("Nasceu em %s", (p1.mes == 3 ? meses[3]: 0) );
+        System.out.printf("Nasceu em %s", (p1.mes == 4 ? meses[4]: 0) );
+        System.out.printf("Nasceu em %s", (p1.mes == 5 ? meses[5]: 0) );
+        System.out.printf("Nasceu em %s", (p1.mes == 6 ? meses[6]: 0) );
+        System.out.printf("Nasceu em %s", (p1.mes == 7 ? meses[7]: 0) );
+        System.out.printf("Nasceu em %s", (p1.mes == 8 ? meses[8]: 0) );
+        System.out.printf("Nasceu em %s", (p1.mes == 9 ? meses[9]: 0) );
+        System.out.printf("Nasceu em %s", (p1.mes == 10 ? meses[10]: 0) );
+        System.out.printf("Nasceu em %s", (p1.mes == 11 ? meses[11]: 0) );
+        System.out.printf("Nasceu em %s", (p1.mes == 12 ? meses[12]: 0) );
+        entrada.close();
+	}
+}
